f9057e6c3eaa457727b7218bd711449ba8c71ee4
shooke <xingjiehu@163.com>
2017-02-15 15:38:21 +0800

    自动载入和异常

--- corephp/base/Autoload.php
+++ corephp/base/Autoload.php
@@ -11,5 +11,60 @@ namespace corephp\base;
 
 class Autoload
 {
+    /**
+     * 载入路径
+     * @var array
+     */
+    private $loadDir = [];
 
+    /**
+     * 初始化自动载入
+     * @param array|string $path
+     */
+    public function __construct($path)
+    {
+        $this->addAutoLoadPath($path);
+        $this->registerAutoload();
+    }
+    /**
+     * 设置自动载入路径
+     * @param array|string $path
+     */
+    public function addAutoLoadPath($path)
+    {
+        //如果是字符串则转换为数组
+        if(is_string($path)){
+            $path = [$path];
+        }
+        //合并载入路径的数组，并且去除重复值
+        $this->loadDir = array_unique(array_merge($this->loadDir, $path));
+    }
+
+    /**
+     * 注册自动加载
+     */
+    private function registerAutoload()
+    {
+        // 注册AUTOLOAD方法
+        spl_autoload_register([$this,'loader']);
+    }
+
+    /**
+     * 实现类的自动加载
+     * @param string $className
+     * @return boolean
+     */
+    private function loader($className)
+    {
+        $loadDir = $this->loadDir;
+        foreach ($loadDir as $dir) {
+            $file = $dir . DIRECTORY_SEPARATOR . $className . '.php';
+            $file = str_replace(['//','\\\\','\\','/'], DIRECTORY_SEPARATOR, $file);//防止出现dir//dir或dir\\dir格式
+            if (file_exists($file)) {
+                require $file;
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- corephp/exception/Exception.php
+++ corephp/exception/Exception.php
@@ -9,7 +9,65 @@
 namespace corephp\exception;
 
 
+
 class Exception
 {
+    /**
+     * 初始化处理
+     */
+    public function register()
+    {
+        ini_set('display_errors', false);
+        set_exception_handler([$this, 'handleException']);
+        set_error_handler([$this, 'handleError']);
+        register_shutdown_function([$this, 'handleFatalError']);
+    }
 
+    /**
+     * 恢复默认处理
+     */
+    public function unregister()
+    {
+        restore_error_handler();
+        restore_exception_handler();
+    }
+
+    /**
+     * 异常处理
+     * @param \Exception $exception
+     */
+    public function handleException($exception)
+    {
+        //恢复默认处理
+        $this->unregister();
+
+        // 通知服务器，500代码错误
+        http_response_code(500);
+
+        echo $exception->getMessage().PHP_EOL.$exception->getTraceAsString();
+
+    }
+
+    /**
+     * 错误处理返回异常
+     * @param $code
+     * @param $message
+     * @param $file
+     * @param $line
+     * @throws \ErrorException
+     */
+    public function handleError($code, $message, $file, $line)
+    {
+        throw new \ErrorException($message, $code, $code, $file, $line);
+    }
+
+    /**
+     * 发生错误返回异常
+     * @throws \ErrorException
+     */
+    public function handleFatalError()
+    {
+        $error = error_get_last();
+        throw new \ErrorException($error['message'], $error['type'], $error['type'], $error['file'], $error['line']);
+    }
 }
