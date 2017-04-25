702152f2b32564bf82f8b839a9141526069d0b2b
shooke <xingjiehu@163.com>
2017-04-25 11:16:14 +0800

    简化object

--- corephp/base/Object.php
+++ corephp/base/Object.php
@@ -8,37 +8,31 @@
 
 namespace corephp\base;
 
-
-use corephp\exception\TypeErrorException;
-
 class Object implements Root
 {
-    public $computed;
-    protected $computedObject;
-    public $watch;
-    public $attributes=[];
-
+    /**
+     * 调用读取方法
+     * @param $name
+     * @return mixed
+     */
     public function __get($name)
     {
-        if(isset($this->$name)) return $this->$name;
-        if(isset($this->attributes[$name])) return $this->attributes[$name];
-        if($this->computed){
-            if(!is_object($this->computedObject)){
-                $computed = new $this->computed();
-                if($computed instanceof Computed){
-                    $computed->caller = $this;
-                    $this->computedObject = $computed;
-                }else{
-                    throw new TypeErrorException('计算属性类未继承 corephp\base\Computed');
-                }
-            }
-            return call_user_func([$this->computedObject,$name]);
+        $getter = 'get' . $name;
+        if (method_exists($this, $getter)) {
+            return $this->$getter();
         }
-        return null;
     }
-    public function __set($name ,$value)
+
+    /**
+     * 调用设置方法
+     * @param $name
+     * @param $value
+     */
+    public function __set($name, $value)
     {
-        if(isset($this->$name)) $this->$name = $value;
-        if(isset($this->attributes[$name])) $this->attributes[$name] = $value;
+        $setter = 'set' . $name;
+        if (method_exists($this, $setter)) {
+            $this->$setter($value);
+        }
     }
 }
