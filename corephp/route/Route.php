458df6f721cc583479bf2f2b7399ef8a37327928
shooke <xingjiehu@163.com>
2017-04-27 16:16:53 +0800

    路由

--- corephp/route/Route.php
+++ corephp/route/Route.php
@@ -11,32 +11,89 @@ namespace corephp\route;
 
 class Route
 {
-    private $rules;
-    public function __construct($rules)
-    {
-        $this->$rules = $rules;
-        $this->_initRule();
+    /**
+     * 中间件
+     * @var array
+     */
+    public $middleware;
+
+    /**
+     * 默认要执行的控制器方法
+     * @var string
+     */
+    public $defaultRun;
+    /**
+     * 控制器命名空间
+     * @var string
+     */
+    public $controllerNamespace;
+
+    /**
+     * 路由规则
+     * @var array
+     */
+    public $rules;
+    
+    public $routeParamName = '';
+
+
+    /**
+     * 获取请求方式
+     * @return mixed
+     */
+    private function _method(){
+        return $_SERVER['REQUEST_METHOD'];
     }
 
-    public function parse()
+    /**
+     * 获取请求的路由
+     * @return mixed
+     */
+    private function _uri()
     {
-
-    }
-    public function createUrlByRoute()
-    {
-
-    }
-    public function createUrlByController()
-    {
-
-    }
-    public function runAction()
-    {
-
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 
-    private function _initRule()
+    private function _runRule()
     {
+        $uri = $this->_uri();
+        $method = $this->_method();
+        $route = $this->defaultRun;//控制器方法
+        $arguments = [];//参数
+        foreach ($this->rules as $rule) {
+            //验证路由规则
+            if (preg_match('#' . $rule['route'] . '#', $uri, $matches)) {
+                //验证请求方式
+                if (isset($rule['method']) && preg_match("/$method/i", $rule['method'])) {
+                    $route = $rule['run'];
+                    array_shift($matches);//移除完整匹配保留参数
+                    $arguments = $matches;
+                    break;
+                }
+            }
+        }
+        return [
+            'route'=>$route,
+            'arguments'=>$arguments
+        ];
+    }
+    /**
+     * Runs the callback for the given request
+     */
+    public function dispatch()
+    {
+        if($this->rules){
+            $call = $this->_runRule();
+        }else{
+
+        }
+
+        $className = preg_replace('/\\+|\/+/', '\\', $this->controllerNamespace.'/'.dirname($call['route']));
+        $controllerObject = new $className;
+        $action = basename($call['route']);
+        return call_user_func_array([$controllerObject,$action],$call['arguments']);
+
 
     }
+
 }
